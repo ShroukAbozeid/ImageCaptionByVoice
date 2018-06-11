b623baeefecb45898de08af228ea08f3150c1b6f
MahmoudHigazy <ma7moudhigazy@gmail.com>
2018-06-11 12:17:33 +0200

    edited for vald1

--- run_inference.py
+++ run_inference.py
@@ -15,20 +15,20 @@ FLAGS = tf.flags.FLAGS
 
 import os
 
-tf.flags.DEFINE_string("test_captions_file", "/tmp/captions_test2014.json",
+tf.flags.DEFINE_string("test_captions_file", "captions_val2014.json",
                        "testing captions JSON file.")
 
-tf.flags.DEFINE_string("checkpoint_path", "/media/higazy/New Volume/GP/ImageCaptionByVoice/model/train/",
+tf.flags.DEFINE_string("checkpoint_path", "./model/train/",
                        "Model checkpoint file or directory containing a "
                        "model checkpoint file.")
 
-tf.flags.DEFINE_string("vocab_file", "./data/word_counts.txt", "Text file containing the vocabulary.")
-tf.flags.DEFINE_string("input_files", "",
+tf.flags.DEFINE_string("vocab_file", "word_counts101.txt", "Text file containing the vocabulary.")
+tf.flags.DEFINE_string("input_files", "./img",
                        "File pattern or comma-serated list of file patterns "
                        "of image files.")
 tf.flags.DEFINE_string(flag_name="rnn_type", default_value="lstm",
                        docstring="RNN cell type lstm/gru .")
-tf.flags.DEFINE_boolean("mode", False, "true for directory and false for file patterns")
+tf.flags.DEFINE_boolean("mode", True, "true for directory and false for file patterns")
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
@@ -45,18 +45,12 @@ def main(_):
 
 
   filenames = []
-  if FLAGS.mode == False:
-    for file_pattern in FLAGS.input_files.split(","):
-        filenames.extend(tf.gfile.Glob(file_pattern))
-    tf.logging.info("Running caption generation on %d files matching %s",
-                  len(filenames), FLAGS.input_files)
 
-  else:
-    path = FLAGS.input_files + "/*.jpg"
-    for image in glob.glob(path):
-        filenames.extend(tf.gfile.Glob(image))
-    tf.logging.info("Running caption generation on %d files matching %s",
-                    len(filenames), FLAGS.input_files)
+  path = FLAGS.input_files + "/*.jpg"
+  for image in glob.glob(path):
+    filenames.extend(tf.gfile.Glob(image))
+  tf.logging.info("Running caption generation on %d files matching %s",
+    len(filenames), FLAGS.input_files)
 
   with tf.Session(graph=g) as sess:
     # Load the model from checkpoint.
@@ -75,15 +69,28 @@ def main(_):
 
     data = []
 
+    with open('val_images.json') as f:
+        val_data = json.load(f)
+    val_image_names = val_data['images_name']
+
     for filename in filenames:
       with tf.gfile.GFile(filename, "rb") as f:
         image = f.read()
       captions = generator.beam_search(sess, image)
       print("Captions for image %s:" % os.path.basename(filename))
       if FLAGS.mode == True:
-          img_id = id_to_filename[filename]
+          img_name = os.path.basename(filename)
+          '''if img_name not in val_image_names:
+              continue'''
+          img_id = id_to_filename[img_name]
           caption = captions[0]
-          sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
+          temp = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
+          sentence = ''
+          for word in temp:
+              if sentence != '':
+                  sentence += ' '
+              for c in word:
+                  sentence += c
           data.append({'image_id' : img_id, 'caption' : sentence})
 
 
@@ -93,7 +100,7 @@ def main(_):
         sentence = " ".join(sentence)
         print("  %d) %s (p=%f)" % (i, sentence, math.exp(caption.logprob)))
 
-    with open('captions_test2014_im2txt_results.json', 'w') as outfile:
+    with open('captions_val2014_im2txt_results.json', 'w') as outfile:
         json.dump(data, outfile)
 
 
